72f19442b2562b4c14cf666dbd86194635ff93d9
adamzz <ademdjellit1@gmail.com>
2025-03-25 10:56:38 +0100

    gh pages automate

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tailwindcss()],
-  base: 'ellabs-roadmap2'
+  base: 'ellabs-roadmap2',
 })
